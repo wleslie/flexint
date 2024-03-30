67da65279649715f1ee106e5d7a4dc596c8faec4
William J. Leslie <will@willleslie.com>
2024-03-30 13:56:20 -0700

    Implement Pow

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use std::{
 };
 
 use num_bigint::{BigInt, BigUint, Sign};
+use num_traits::{CheckedMul, One, Pow};
 
 // Invariant: 'Small' must be used when value fits. 'Big' shall be used *only* when value does not
 // fit in 'Small'
@@ -35,13 +36,13 @@ macro_rules! ref_deref {
 
 /// Implements `[&]$Flex ⋄ &$Small` by delegating to `$checked_op` and/or `$Big ⋄ $Small`.
 macro_rules! binop_flex_small {
-    ($Op:ident::$op:ident, $Flex:ty $(as $lhs_ref:tt)?, $Small:ty, $Big:ty, $checked_op:ident $(,)?) => {
+    ($Op:ident::$op:ident, $Flex:ty $(as $lhs_ref:tt)?, $Small:ty, $Big:ty, $checked_op:expr $(,)?) => {
         impl $Op<&$Small> for ref_borrow!($($lhs_ref)? $Flex) {
             type Output = $Flex;
             fn $op(self, rhs: &$Small) -> Self::Output {
                 match ref_borrow!($($lhs_ref)? self.0) {
                     Inner::Small(a) => {
-                        if let Some(n) = a.$checked_op(*rhs) {
+                        if let Some(n) = $checked_op(ref_deref!($($lhs_ref)? a), *rhs) {
                             n.into()
                         } else {
                             $Op::$op(<$Big>::from(ref_deref!($($lhs_ref)? a)), *rhs).into()
@@ -53,17 +54,18 @@ macro_rules! binop_flex_small {
         }
     };
 }
+
 /// Implements `$Flex ⋄= [&]$Small` by delegating to `$checked_op` and/or `$Big ⋄= $Small`.
 macro_rules! assign_flex_small {
     (
         $Op:ident::$op:ident, $Flex:ty, $Small:ty $(as $rhs_ref:tt)?,
-        $Big:ty, $checked_op:ident, $Binop:ident::$binop:ident $(,)?
+        $Big:ty, $checked_op:expr, $Binop:ident::$binop:ident $(,)?
     ) => {
         impl $Op<ref_borrow!($($rhs_ref)? $Small)> for $Flex {
             fn $op(&mut self, rhs: ref_borrow!($($rhs_ref)? $Small)) {
                 match &mut self.0 {
                     Inner::Small(a) => {
-                        if let Some(n) = a.$checked_op(ref_deref!($($rhs_ref)? rhs)) {
+                        if let Some(n) = $checked_op(*a, ref_deref!($($rhs_ref)? rhs)) {
                             *a = n;
                         } else {
                             *self = $Binop::$binop(<$Big>::from(*a), ref_deref!($($rhs_ref)? rhs)).into();
@@ -95,6 +97,22 @@ macro_rules! binop_flex_t {
         }
     };
 }
+
+/// Implements `[&]$Flex ⋄ T` by delegating to `[&]$Big ⋄ T`.
+macro_rules! binop_flex_t_via_big {
+    ($Op:ident::$op:ident, $Flex:ty $(as $lhs_ref:tt)?, $Rhs:ty, $Big:ty $(,)?) => {
+        impl $Op<$Rhs> for ref_borrow!($($lhs_ref)? $Flex) {
+            type Output = $Flex;
+            fn $op(self, rhs: $Rhs) -> Self::Output {
+                match ref_borrow!($($lhs_ref)? self.0) {
+                    Inner::Small(a) => $Op::$op(<$Big>::from(ref_deref!($($lhs_ref)? a)), rhs),
+                    Inner::Big(a) => $Op::$op(a, rhs),
+                }.into()
+            }
+        }
+    };
+}
+
 /// Implements `$Flex ⋄= T` by delegating to either `$Small ⋄ T` or `$Big ⋄= T`.
 macro_rules! assign_flex_t {
     ($Op:ident::$op:ident, $Flex:ty, $Rhs:ty $(as $rhs_ref:tt)?, $Small:ty, $Binop:ident::$binop:ident $(,)?) => {
@@ -116,9 +134,9 @@ macro_rules! assign_flex_t {
 
 /// Implements `T ⋄ [&]$Flex` by delegating to either `T ⋄ [&]$Small` or `T ⋄ [&]$Big`.
 macro_rules! binop_t_flex {
-    ($Op:ident::$op:ident, $Lhs:ty, $Flex:ty $(as $rhs_ref:tt)? $(,)?) => {
+    ($Op:ident::$op:ident, $Lhs:ty, $Flex:ty $(as $rhs_ref:tt)?, $Output:ty $(,)?) => {
         impl $Op<ref_borrow!($($rhs_ref)? $Flex)> for $Lhs {
-            type Output = $Flex;
+            type Output = $Output;
             fn $op(self, rhs: ref_borrow!($($rhs_ref)? $Flex)) -> Self::Output {
                 match ref_borrow!($($rhs_ref)? rhs.0) {
                     Inner::Small(b) => $Op::$op(self, b),
@@ -128,6 +146,7 @@ macro_rules! binop_t_flex {
         }
     };
 }
+
 /// Implements `T ⋄= [&]$Flex` by delegating to either `T ⋄= [&]$Small` or `T ⋄= [&]$Big`.
 macro_rules! assign_t_flex {
     ($Op:ident::$op:ident, $Lhs:ty, $Flex:ty $(as $rhs_ref:tt)? $(,)?) => {
@@ -144,13 +163,13 @@ macro_rules! assign_t_flex {
 
 macro_rules! binop_family_with_assign {
     (
-        $Op:ident::$op:ident, $OpAssign:ident::$op_assign:ident, $checked_op:ident,
+        $Op:ident::$op:ident, $OpAssign:ident::$op_assign:ident, $checked_op:expr,
         $Flex:ty, $Small:ty, $Big:ty $(,)?
     ) => {
         // &$Flex ⋄ &T
         binop_flex_small!($Op::$op, $Flex as ref, $Small, $Big, $checked_op);
         binop_flex_t!($Op::$op, $Flex as ref, &$Big);
-        binop_t_flex!($Op::$op, &$Flex, $Flex as ref);
+        binop_t_flex!($Op::$op, &$Flex, $Flex as ref, $Flex);
 
         // $Flex ⋄= &T
         assign_flex_small!($OpAssign::$op_assign, $Flex, $Small as ref, $Big, $checked_op, $Op::$op);
@@ -165,28 +184,28 @@ macro_rules! binop_family_with_assign {
         // $Flex ⋄ &T
         binop_flex_small!($Op::$op, $Flex, $Small, $Big, $checked_op);
         binop_flex_t!($Op::$op, $Flex, &$Big);
-        binop_t_flex!($Op::$op, $Flex, $Flex as ref);
+        binop_t_flex!($Op::$op, $Flex, $Flex as ref, $Flex);
 
         // &$Flex ⋄ T
         trait_tactics::binop_via_binop_ref_rhs!(impl $Op<$Small> for &$Flex { fn $op -> $Flex });
         binop_flex_t!($Op::$op, $Flex as ref, $Big);
-        binop_t_flex!($Op::$op, &$Flex, $Flex);
+        binop_t_flex!($Op::$op, &$Flex, $Flex, $Flex);
 
         // $Flex ⋄ T
         trait_tactics::binop_via_assign!(impl $Op<$Small> for $Flex { fn $op => $OpAssign::$op_assign });
         binop_flex_t!($Op::$op, $Flex, $Big);
-        binop_t_flex!($Op::$op, $Flex, $Flex);
+        binop_t_flex!($Op::$op, $Flex, $Flex, $Flex);
     };
 }
 
 macro_rules! impl_neg {
-    ($Flex:ty $(as $ref:tt)?, $Big:ty, $checked_op:ident $(,)?) => {
+    ($Flex:ty $(as $ref:tt)?, $Big:ty, $checked_op:expr $(,)?) => {
         impl Neg for ref_borrow!($($ref)? $Flex) {
             type Output = $Flex;
             fn neg(self) -> Self::Output {
                 match ref_borrow!($($ref)? self.0) {
                     Inner::Small(a) => {
-                        if let Some(n) = a.$checked_op() {
+                        if let Some(n) = $checked_op(ref_deref!($($ref)? a)) {
                             n.into()
                         } else {
                             (-<$Big>::from(ref_deref!($($ref)? a))).into()
@@ -196,7 +215,7 @@ macro_rules! impl_neg {
                 }
             }
         }
-            };
+    };
 }
 
 macro_rules! flex_type {
@@ -275,15 +294,55 @@ macro_rules! flex_type {
             }
         }
 
-        binop_family_with_assign!(Add::add, AddAssign::add_assign, checked_add, $Flex, $Small, $Big);
-        binop_family_with_assign!(Sub::sub, SubAssign::sub_assign, checked_sub, $Flex, $Small, $Big);
-        binop_family_with_assign!(Mul::mul, MulAssign::mul_assign, checked_mul, $Flex, $Small, $Big);
+        binop_family_with_assign!(Add::add, AddAssign::add_assign, <$Small>::checked_add, $Flex, $Small, $Big);
+        binop_family_with_assign!(Sub::sub, SubAssign::sub_assign, <$Small>::checked_sub, $Flex, $Small, $Big);
+        binop_family_with_assign!(Mul::mul, MulAssign::mul_assign, <$Small>::checked_mul, $Flex, $Small, $Big);
         // Possible optimization: panic early when the divisor is zero, instead of letting
         // checked_div/checked_rem fail and falling back to bignum operations that ultimately panic
         // anyway.
-        binop_family_with_assign!(Div::div, DivAssign::div_assign, checked_div, $Flex, $Small, $Big);
-        binop_family_with_assign!(Rem::rem, RemAssign::rem_assign, checked_rem, $Flex, $Small, $Big);
+        binop_family_with_assign!(Div::div, DivAssign::div_assign, <$Small>::checked_div, $Flex, $Small, $Big);
+        binop_family_with_assign!(Rem::rem, RemAssign::rem_assign, <$Small>::checked_rem, $Flex, $Small, $Big);
+
+        // Pow is special because the RHS is always unsigned. Also, there's no PowAssign
+
+        // &$Flex ** &T
+        binop_flex_small!(Pow::pow, $Flex as ref, u64, $Big, checked_pow);
+        binop_flex_t_via_big!(Pow::pow, $Flex as ref, &BigUint, $Big);
+        binop_t_flex!(Pow::pow, &$Flex, FlexUint as ref, $Flex);
+
+        // $Flex ** &T
+        binop_flex_small!(Pow::pow, $Flex, u64, $Big, checked_pow);
+        binop_flex_t_via_big!(Pow::pow, $Flex, &BigUint, $Big);
+        binop_t_flex!(Pow::pow, $Flex, FlexUint as ref, $Flex);
+
+        // &$Flex ** T
+        trait_tactics::binop_via_binop_ref_rhs!(impl Pow<u64> for &$Flex { fn pow -> $Flex });
+        binop_flex_t_via_big!(Pow::pow, $Flex as ref, BigUint, $Big);
+        binop_t_flex!(Pow::pow, &$Flex, FlexUint, $Flex);
+
+        // $Flex ** T
+        trait_tactics::binop_via_binop_ref_rhs!(impl Pow<u64> for $Flex { fn pow -> $Flex });
+        binop_flex_t_via_big!(Pow::pow, $Flex, BigUint, $Big);
+        binop_t_flex!(Pow::pow, $Flex, FlexUint, $Flex);
     };
+}
+
+fn checked_pow<T: One + CheckedMul>(mut base: T, mut exp: u64) -> Option<T> {
+    // Exponentiation by squaring; essentially the same algorithm as used by
+    // `{integer}::checked_pow` and `num_traits::checked_pow`, but the exponent type is u64, instead
+    // of u32 or usize (respectively).
+    if exp == 0 {
+        return Some(T::one());
+    }
+    let mut acc = T::one();
+    while exp > 1 {
+        if (exp & 1) != 0 {
+            acc = acc.checked_mul(&base)?;
+        }
+        exp /= 2;
+        base = base.checked_mul(&base)?;
+    }
+    acc.checked_mul(&base)
 }
 
 flex_type!(
@@ -298,5 +357,17 @@ flex_type!(
     cmp_small_big = |big| Sign::NoSign.cmp(&big.sign()),
 );
 
-impl_neg!(FlexInt, BigInt, checked_neg);
-impl_neg!(FlexInt as ref, BigInt, checked_neg);
+// For obvious reasons, these are only implemented for signed integers
+impl_neg!(FlexInt, BigInt, i64::checked_neg);
+impl_neg!(FlexInt as ref, BigInt, i64::checked_neg);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_checked_pow() {
+        assert_eq!(checked_pow(3u64, 19), Some(1162261467));
+        assert_eq!(checked_pow(5u64, 12), Some(244140625));
+    }
+}
